e29eea7cc5ce370f88415a93c34281d407734dfe
Sahil Shinde <sahilshinde9229@gmail.com>
2026-02-14 17:16:07 +0530

    Files Added

---
+++ unused/stt.py
@@ -0,0 +1,24 @@
+import whisper
+import json
+
+model = whisper.load_model("small")
+
+result = model.transcribe(audio="audios/13_Entities, Code tag and more on HTML.mp3",
+                          language="hi",
+                          task="translate",
+                          word_timestamps=False)
+
+print(result["segments"])
+
+chunks = []
+for segment in result["segments"]:
+    chunks.append({
+        "start": segment["start"],
+        "end": segment["end"],
+        "text": segment["text"]
+    })
+    
+print(chunks)
+
+with open("output1.json", "w") as f:
+    json.dump(chunks, f)
