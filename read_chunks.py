9ccff6ed78c47eebeeaf35a054544a59aaa63d0c
Sahil Shinde <sahilshinde9229@gmail.com>
2026-02-18 17:57:47 +0530

    Updated

--- read_chunks.py
+++ read_chunks.py
@@ -1,8 +1,37 @@
 import requests
+import os
+import json
+import pandas as pd
 
-r = requests.post("http://localhost:11434/api/embeddings", json={
-    "model": "bge-m3",
-    "prompt": "Sahil is a good boy"
-})
+def create_embeddings(text_list):
+    r = requests.post("http://localhost:11434/api/embed", json={
+        "model": "bge-m3",
+        "input": text_list
+    })
+    
+    embedding = r.json()["embeddings"]
+    return embedding
 
-print(r.json())
+
+jsons = os.listdir("jsons")  # List all files in the "jsons" directory
+my_dict =[]
+chunk_id =0
+
+for json_file in jsons:
+    with open(f"jsons/{json_file}") as f:
+        content = json.load(f)
+    print(f"Creating embeddings for {json_file} with {len(content['chunks'])} chunks.")
+    embeddings = create_embeddings([ c["text"] for c in content["chunks"]])
+
+    for i, chunk in enumerate(content["chunks"]):
+        chunk["chunk_id"] = chunk_id
+        chunk["embedding"] = embeddings[i]
+        chunk_id +=1
+        my_dict.append(chunk)
+
+# print(my_dict)
+
+df = pd.DataFrame.from_records(my_dict)
+print(df)
+# a = create_embeddings(["hello world", "how are you?"])
+# print(a)
